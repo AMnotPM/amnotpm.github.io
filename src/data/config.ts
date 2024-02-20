2ec51d5b6d117590e72fb2b63286a03691fbfe90
Miguel Gutierrez <98622966+AMnotPM@users.noreply.github.com>
2024-02-20 16:02:35 -0500

    update config.ts

--- src/data/config.ts
+++ src/data/config.ts
@@ -11,9 +11,9 @@ const config = {
     },
   },
   meta: {
-    title: 'Mark Freeman - Senior React Developer',
+    title: 'Miguel Gutierrez - Junior Developer in ASP.NET Core',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sodales ac dui at vestibulum. In condimentum metus id dui tincidunt, in blandit mi vehicula.',
+      'A baldly backend developer',
     faviconPath: '/src/assets/my-image.jpeg',
   },
   pdf: {
